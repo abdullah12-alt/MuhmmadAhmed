1fe5015ca9085cfc5615e578652db6060454890f
Abdullah <hassanabdullah@gmail.com>
2024-01-08 12:24:55 +0500

    project code

---
+++ day4.py
@@ -0,0 +1,39 @@
+# string="Hello world"
+# print(string[::-1])
+
+
+# li=["apple","banana","gava"]
+# print(li)
+# print(type(li))
+# print(li[0])
+
+# li.append("cherry")
+# print(li)
+
+# print(len(li))
+# li.append(True)
+# print(li)
+
+# fruite=["apple","banana","mangos","cherry","gava"]
+
+# inp=input("Enter the friute name : ")
+# if inp in fruite:
+#     print(f"Yes we have {inp}.")
+# else:
+#     print(f"sorry we don't have {inp}")
+
+# fruite=["apple","banana","mangos","cherry","gava"]
+
+# # fruite[1:2]="graps"
+# fruite[1:3]
+# print(fruite[1:3])
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(2, "watermelon")
+# print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+thislist.remove("banana")
+print(thislist)
---
+++ firstProject.py
@@ -0,0 +1,12 @@
+print("############ Welcome to The Mad Lib Game ############ ")
+
+adjective=input("Enter the Adjective : ")
+noun=input("Enter the Noun : ")
+verb=input("Enter the verb : ")
+adverb=input("Enter the adverb : ")
+
+
+mad_lib_story = f"Once upon a time, there was a, {adjective} {noun} who loved to {verb} {adverb}."
+
+print("Your Mad Lib story is here ")
+print(mad_lib_story)
