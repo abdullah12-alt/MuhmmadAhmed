d7b7ca705940b929ca78065c488adab471b5f788
Abdullah <hassanabdullah@gmail.com>
2024-01-18 12:09:12 +0500

    all code

--- day8.py
+++ day8.py
@@ -1,4 +1,25 @@
-i=1
-while(i<=10):
-    print(i)
-    i=i+1
+# num=int(input("Enter the number : "))
+
+# i =1
+# while(i<=10):
+#     print(f"{num} * {i} = {i*num}")
+#     i=i+1 
+
+
+# i =1;
+# while i<50:
+#     if i==25:
+#         print("Oh i can't move more ")
+#         break
+#     print(i)
+#     i=i+1
+    
+# i =1;
+# while i<=50:
+   
+#     print(i)
+    
+#     i=i+1
+#     if i==25:
+#         print("skip 25")
+#         continue
